9d60b67abc859a6a1a8357382a868904d6b56982
hansigaya99 <hansigaya99@gmail.com>
2023-09-15 01:21:41 +0530

    Added visualize function

--- infovis.py
+++ infovis.py
@@ -12,3 +12,23 @@ def fetch_attendance(student_id):
     data = c.fetchall()
     conn.close()
     return data
+
+def visualize(data, student_id):
+    statuses = [x[0] for x in data]
+    counts = [x[1] for x in data]
+    plt.bar(statuses, counts)
+    plt.title(f'Attendance Summary for {student_id}')
+    plt.xlabel('Status')
+    plt.ylabel('Count')
+    plt.show()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python infovis.py <student_id>")
+        sys.exit(1)
+
+    student_id = sys.argv[1]
+    data = fetch_attendance(student_id)
+    visualize(data, student_id)
